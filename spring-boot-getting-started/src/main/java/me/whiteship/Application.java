51c311c335abcbe1e64c2cd5328c5a744811af4d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-02 21:55:42 +0900

    내장 웹 서버 응용 1부: 컨테이너와 포트

--- spring-boot-getting-started/src/main/java/me/whiteship/Application.java
+++ spring-boot-getting-started/src/main/java/me/whiteship/Application.java
@@ -1,23 +1,12 @@
 package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);
-        springApplication.run(args);
+        SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-    public Holoman holoman() {
-        Holoman holoman = new Holoman();
-        holoman.setName("whiteship");
-        holoman.setHowLong(60);
-        return holoman;
-    }
 }
